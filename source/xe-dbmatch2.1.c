697aea264bb1fb6e2f44ae0730f6341bfb4f270b
JohnHuxter@Molasses <j.r.huxter@gmail.com>
2020-07-11 00:24:03 +0100

    xe-dbmatch2: now can append multiple columns xf_strselect1: prevent trailing delimiters from being output

--- source/xe-dbmatch2.1.c
+++ source/xe-dbmatch2.1.c
@@ -30,7 +30,6 @@ long *xf_lineparse2(char *line,char *delimiters, long *nwords);
 char *xf_strescape1(char *line);
 char *xf_strcat1(char *string1,char *string2,char *delimiter);
 int xf_strselect1(char *input, char delimiter, long *words, long nwords);
-
 /* external functions end */
 
 int main (int argc, char *argv[]) {
@@ -39,9 +38,10 @@ int main (int argc, char *argv[]) {
 	long ii,jj,kk,nn,mm,col,maxlinelen=0;
 	FILE *fpin,*fpout;
 	/* program-specific variables */
-	char *allkeys=NULL,*allvals=NULL,delimout[4];
-	long *iwords=NULL,*ikeys=NULL,*ivals=NULL,nheader=-1,nwords=0,nkeylines=0;
-	long incol=-1,keycol=-1,valcol=-1;
+	char *allkeys=NULL,*allvals=NULL,*valheader=NULL,*psetval=NULL,delimout[4];
+	long *iwords=NULL,*ikeys=NULL,*ivals=NULL,nheader=-1,nwords=0,nkeylines=0,linelen;
+	long *isetval=NULL,*valcol=NULL,nsetval=0;
+	long incol=-1,keycol=-1,xvalcol=-1;
 	/* arguments */
 	char *infile=NULL,*keyfile=NULL,*setcol=NULL,*setkey=NULL,*setval=NULL,*setdelim=NULL,*setmissing=NULL;
 	int setskip=1;
@@ -54,28 +54,27 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr,"----------------------------------------------------------------------\n");
 		fprintf(stderr,"%s\n",TITLE_STRING);
 		fprintf(stderr,"----------------------------------------------------------------------\n");
-		fprintf(stderr,"Read an input and append the matching value from a keyfile\n");
-		fprintf(stderr,"- match is performed on a column from the input and the keyfile\n");
-		fprintf(stderr,"- the appended value is in a specified column in the keyfile\n");
+		fprintf(stderr,"Read input-1 and append the corresponding values from a input-2\n");
+		fprintf(stderr,"- match is performed on a column (key) from both files\n");
 		fprintf(stderr,"- if there is no matching key, a \"-\" will be appended instead\n");
-		fprintf(stderr,"- matches are case-sensitive and must be exact (eg. 3 does not equal 03)\n");
+		fprintf(stderr,"- matches are case-sensitive & must be exact (e.g. 3 doesn't match 03)\n");
 		fprintf(stderr,"USAGE:\n");
-		fprintf(stderr,"	%s [input] [col] [keyfile] [val]  [options]\n",thisprog);
-		fprintf(stderr,"	[input]: file name or \"stdin\" with key-column to be matched\n");
-		fprintf(stderr,"	[col]: input column to match with key\n");
-		fprintf(stderr,"	[keyfile]: file with key- and value-columns\n");
-		fprintf(stderr,"	[val]: column in keyfile containing values to append\n");
+		fprintf(stderr,"	%s [in1] [col] [in2] [val]  [options]\n",thisprog);
+		fprintf(stderr,"	[in1]: the file (or \"stdin\") to be added to\n");
+		fprintf(stderr,"	[col]: in1 column-name holding the keys\n");
+		fprintf(stderr,"	[in2]: file containing values to be appended\n");
+		fprintf(stderr,"	[vals]: in2 column-names (CSV) to append\n");
 		fprintf(stderr,"VALID OPTIONS:\n");
-		fprintf(stderr,"	[-k]: key-column to match in keyfile [ default=[col] ]\n");
+		fprintf(stderr,"	[-k]: key-column to match in in2 [ default=[col] ]\n");
 		fprintf(stderr,"	[-m]: missing key-match placeholder to append [\"-\"]\n");
 		fprintf(stderr,"	[-d]: characters to use as column-delimiters [\"\\t\"]\n");
 		fprintf(stderr,"		- max %d characters permitted\n",MAXDELIM);
-		fprintf(stderr,"		- the first delimiter is used for the output\n");
 		fprintf(stderr,"		- any delimiter in the input marks a new column\n");
-		fprintf(stderr,"EXAMPLES: add group-names to a file with only group-no. specified\n");
-		fprintf(stderr,"	%s data.txt group names.txt name -d \",\"\n",thisprog);
+		fprintf(stderr,"		- the first delimiter is used for the output\n");
+		fprintf(stderr,"EXAMPLES: add age and height to a file containing names and sex\n");
+		fprintf(stderr,"	%s names.txt NAME stats.txt AGE,HEIGHT -d \",\"\n",thisprog);
 		fprintf(stderr,"OUTPUT:\n");
-		fprintf(stderr,"	input + matching values from keyfile appended on each line\n");
+		fprintf(stderr,"	in1 + matching values from in2 appended on each line\n");
 		fprintf(stderr,"----------------------------------------------------------------------\n");
 		fprintf(stderr,"\n");
 		exit(0);
@@ -111,6 +110,12 @@ int main (int argc, char *argv[]) {
 	}
 	sprintf(delimout,"%c",setdelim[0]);
 
+	/* PARSE SETVAL INTO INDIVIDUAL VALUE-WORDS, AND RESERVE MEMORY FOR ASSOCIATED COLUMNS  */
+	isetval= xf_lineparse2(setval,",",&nsetval);
+	if(nsetval<1) {fprintf(stderr,"\n--- Error[%s]: no value-columns defined\n\n",thisprog);exit(1);}
+	valcol= realloc(valcol,nsetval*sizeof(*valcol));
+	if(valcol==NULL) {fprintf(stderr,"\n--- Error[%s]: insufficient memory\n\n",thisprog);exit(1);}
+	//TEST for(ii=0;ii<nsetval;ii++) printf("%ld\t%s\tcolumn: %ld\n",ii,(setval+isetval[ii]),valcol[ii]); exit(0);
 
 	/********************************************************************************/
 	/* STORE KEY-VALUE PAIRS */
@@ -119,71 +124,56 @@ int main (int argc, char *argv[]) {
 	nn= mm= nkeylines= 0;
 	while((line= xf_lineread1(line,&maxlinelen,fpin))!=NULL) {
 		if(maxlinelen==-1)  {fprintf(stderr,"\n--- Error[%s]: readline function encountered insufficient memory\n\n",thisprog);exit(1);}
+		linelen= strlen(line);
 		mm++; // just used for error-reporting
- 		/* skip blank or commented lines */
-		if(setskip==1 && (line[0]=='#'||strlen(line)<2)) continue;
+ 		if(setskip==1 && (line[0]=='#'||strlen(line)<2)) continue; // skip blank or commented lines */
+		else nn++; // actual record-number
 		/* parse the line */
 		iwords= xf_lineparse2(line,setdelim,&nwords);
-
-		/* if it's the header-line, determine the key-column */
-		if(++nn==1) {
-			/* record how many columns there should be */
-			nheader= nwords;
-			/* determine the key-column and value-column to match */
-			for(ii=0;ii<nwords;ii++) {
-				if(strcmp(setkey,line+iwords[ii])==0) keycol=ii;
-				if(strcmp(setval,line+iwords[ii])==0) valcol=ii;
-			}
-
-			// ??? NEXT STEP - REPLACE THE CODE ABOVE WITH A MULTI-KEY SEARCH AND STORE THE LIST OF KEYCOLS
-			// ?? KEYCOLS SHOULD BE INITIALISED TO -1 - TEST AFTER SEARCH
-			// /* look for matches between words and keys */
-			// for(jj=0;jj<nwords;jj++) {
-			// 	pword= (line+iwords[jj]);
-			// 	for(kk=0;kk<nkeys;kk++) if(strcmp(key[kk],pword)==0) keycol[kk]= jj;
-			// }
-
-
+		/* if it's the header-line, determine the value-columns, and keep a value-header */
+		if(nn==1) {
+			nheader= nwords; // record how many columns there should be
+			/* determine the key-column to match */
+			for(ii=0;ii<nwords;ii++) if(strcmp(setkey,line+iwords[ii])==0) keycol=ii;
 			if(keycol==-1) {fprintf(stderr,"\n--- Error[%s]: no key-column matching \"%s\" found in %s\n\n",thisprog,setkey,keyfile);exit(1);}
-			if(valcol==-1) {fprintf(stderr,"\n--- Error[%s]: no value-column matching \"%s\" found in %s\n\n",thisprog,setval,keyfile);exit(1);}
+			/* build the array of value-columns to match */
+			for(jj=0;jj<nsetval;jj++) {
+				valcol[jj]=-1;
+				psetval= setval+isetval[jj];
+				for(ii=0;ii<nwords;ii++) if(strcmp(psetval,line+iwords[ii])==0) valcol[jj]= ii;
+				if(valcol[jj]==-1) {fprintf(stderr,"--- Warning[%s]: no value-column matching \"%s\" found in %s\n",thisprog,psetval,keyfile);}
+			}
+			/* unparse the line (replace all nulls before the end with the primary setdelim) */
+			for(ii=0;ii<linelen;ii++) if(line[ii]=='\0') line[ii]= setdelim[0];
+			/* use strselect to reduce the line to the set values of interest, delimited */
+			ii= xf_strselect1(line,setdelim[0],valcol,nsetval);
+			/* copy this selected line to valheader for use with output */
+			valheader= realloc(valheader,linelen*sizeof(*valheader));
+			if(valheader==NULL) {fprintf(stderr,"\n--- Error[%s]: insufficient memory\n\n",thisprog);exit(1);}
+			strncpy(valheader,line,linelen);
 			continue;
 		}
-
 		/* for all other lines, store the key and value, if the appropriate columns are present */
-		if(nwords!=nheader) {fprintf(stderr,"\n--- Error[%s]: corrupt key line %ld - number of columns (%ld) does not match number of headers (%ld) in %s\n\n",thisprog,mm,nwords,nheader,keyfile);exit(1);}
-		allkeys= xf_strcat1(allkeys,line+iwords[keycol],setdelim);
-		allvals= xf_strcat1(allvals,line+iwords[valcol],setdelim);
-
-		// REPLACE THE ABOVE CODE WITH STORAGE OF A SELECTED VERSION OF EACH LINE AS IN THIS EXAMPLE
-			// char input[]= "zero,one,two,three,four,five,siz,seven\n";
-			// long ii,keep[]= {1,3,5};
-			// ii= xf_strselect1(input,',',keep,3);
-			// printf("input=%s\n",input);
-
-		// NOTE HOWEVER! WE NEED A SPECIAL DELIMITER TO GO BETWEEN EACH SET OF VALUES!
-		// BEST SOLUTION:
-		// - process allkeys as above, and parse afterwards as always
-		// - unparse the line (replace all nulls before the end with setdelim)
-		// - use strselect to reduce the line
-		// - calculate required storage - do realloc()
-		// - add the "selected" line to the allvals array
-
-
+		if(nwords!=nheader) {fprintf(stderr,"\n--- Error[%s]: corrupt keyfile line %ld - number of columns (%ld) does not match number of headers (%ld) in %s\n\n",thisprog,mm,nwords,nheader,keyfile);exit(1);}
+		/* build the list of keys, make it tab-delimited */
+		allkeys= xf_strcat1(allkeys,line+iwords[keycol],"\n");
+		/* now as for the header line, unparse the line and select the valid columns */
+		for(ii=0;ii<linelen;ii++) if(line[ii]=='\0') line[ii]= setdelim[0];
+		ii= xf_strselect1(line,setdelim[0],valcol,nsetval);
+		/* add the "selected" line to the allvals array, using newline as the delimiter */
+		allvals= xf_strcat1(allvals,line,"\n");
+		/* increment the counter for the number of non-header lines in the key-file */
 		nkeylines++;
 	}
 	fclose(fpin);
 	if(nkeylines==0) {fprintf(stderr,"\n--- Error[%s]: no key-value pairs in %s\n\n",thisprog,keyfile);exit(1);}
 	//TEST: printf("nkeylines=%ld\nallkeys=%s\nallvals=%s\n",nkeylines,allkeys,allvals);
+	//TEST: for(jj=0;jj<nsetval;jj++) printf("valcol[%ld]= %ld\n",jj,valcol[jj]); exit(0);
 
-
-
-	/********************************************************************************/
-	/* PARSE THE MULTI-LINE KEY AND VALUE STRINGS
-	/********************************************************************************/
-	ikeys= xf_lineparse2(allkeys,setdelim,&nn);
-	ivals= xf_lineparse2(allvals,setdelim,&mm);
-	//TEST:for(ii=0;ii<nkeylines;ii++) printf("%ld  %s  %s\n",ii,allkeys+ikeys[ii],allvals+ivals[ii]);
-
+	/* PARSE THE MULTI-LINE KEY AND VALUE STRINGS - REPLACE THE NEWLINES WITH NULLS */
+	ikeys= xf_lineparse2(allkeys,"\n",&nn);
+	ivals= xf_lineparse2(allvals,"\n",&mm);
+	//TEST:for(ii=0;ii<nkeylines;ii++) printf("%ld  %s  %s\n",ii,allkeys+ikeys[ii],allvals+ivals[ii]); exit(0);
 
 
 	/********************************************************************************/
@@ -207,7 +197,8 @@ int main (int argc, char *argv[]) {
 			for(ii=0;ii<nwords;ii++) if(strcmp(setcol,line+iwords[ii])==0) incol=ii;
 			if(incol==-1) {fprintf(stderr,"\n--- Error[%s]: no input-column matching \"%s\" found in %s\n\n",thisprog,setcol,infile);exit(1);}
 			/* output header line, including a column-header for the appended value */
-			for(ii=0;ii<nwords;ii++) printf("%s%s",line+iwords[ii],setdelim); printf("%s\n",setval);
+			for(ii=0;ii<nwords;ii++) printf("%s%s",line+iwords[ii],setdelim);
+			printf("%s\n",valheader);
 			continue;
 		}
 
@@ -218,7 +209,8 @@ int main (int argc, char *argv[]) {
 		/* get pointer to the input-column-to-match */
 		pword= line+iwords[incol];
 		/* find matching keys - the value for the last matching key will be the one used */
-		for(kk=-1,ii=0;ii<nkeylines;ii++) if(strcmp(pword,allkeys+ikeys[ii])==0) kk=ii;
+		kk= -1;
+		for(ii=0;ii<nkeylines;ii++) if(strcmp(pword,allkeys+ikeys[ii])==0) kk=ii;
 		if(kk!=-1) printf("%s\n",allvals+ivals[kk]);
 		else printf("%s\n",setmissing);
 	}
@@ -227,6 +219,9 @@ int main (int argc, char *argv[]) {
 
 	if(line!=NULL) free(line);
 	if(iwords!=NULL) free(iwords);
+	if(isetval!=NULL) free(isetval);
+	if(valcol!=NULL) free(valcol);
+	if(valheader!=NULL) free(valheader);
 	exit(0);
 
 	}
--- source/xf_strselect1.c
+++ source/xf_strselect1.c
@@ -3,6 +3,8 @@
 
 DESCRIPTION:
 	Reduce a string by keeping only a set of delimited "words"
+	Selected words are referred to by indices, range= 0 to (nwords-1)
+	NOTE: the user-defined order of the indices does not change the order of the words in the result
 
 DEPENDENCIES:
 	None
@@ -10,7 +12,7 @@ DEPENDENCIES:
 ARGUMENTS:
 	char *input : pointer to a character array
 	char delimiter : a single delimiter which separates "input" into words
-	long *index1 ; a list (zer0-offset array) of the words in "input" to be kept
+	long *index1 ; a list (zer0-offset array) of the words in "input" to be kept (the order is irrelevant)
 	long nn ; size of the index1 array
 
 RETURN VALUE:
@@ -18,26 +20,23 @@ RETURN VALUE:
 
 SAMPLE CALL:
 	char input[]= "zero,one,two,three,four,five,siz,seven\n";
-	long ii,keep[]= {1,3,5};
-	ii= xf_strselect1(input,',',keep,3);
+	long zz,keep[]= {1,3,5};
+	zz= xf_strselect1(input,',',keep,3);
 	printf("input=%s\n",input);
-
+	exit(0);
 --------------------------------------------------------------------------------
 */
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 
 int xf_strselect1(char *input, char delimiter, long *index1, long nn) {
 	int found;
-	long ii,jj,kk,word,len1,indexmax=-1;
+	long ii,jj,kk,word,len1;
 
 	/* DETERMINE LENGTH OF INPUT */
 	len1= strlen(input); if(len1<1) return(-1);
 
-	/* DETERMINE THE HIGEST INDEX - ERROR IF ANY INDEX IS <0 */
-	for(ii=0;ii<nn;ii++) if(index1[ii]>indexmax) indexmax=index1[ii];
 
 	word=0; // at the start, we assume this is word-0
 	kk=0;	// pointer to write-position in input
@@ -52,12 +51,15 @@ int xf_strselect1(char *input, char delimiter, long *index1, long nn) {
 					kk++;
 					if(input[ii]==delimiter) {
 						word++;
-						if(word>=indexmax) input[ii]='\0';
 						break;
-			}}}
+					}
+				}
+			}
 			if(found==1) break;
-	}}
+		}
+	}
 
 	if(kk<len1) input[kk]='\0';
+	if(input[kk-1]==delimiter) input[kk-1]='\0';
 	return(0);
 }
