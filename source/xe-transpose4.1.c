11ac1c7f7a7b8266ac896a323d8077ee390ea2bc
JohnHuxter@MOLASSES-HP-Z440-Workstation <j.r.huxter@gmail.com>
2021-01-26 23:54:26 +0000

    M source/xe-transpose4.1.c

--- source/xe-transpose4.1.c
+++ source/xe-transpose4.1.c
@@ -10,12 +10,15 @@
 
 /*
 <TAGS>math stats</TAGS>
+
+v 7: 26.January.2021 [JRH]
+	- add control of decimal precision (-p) for repeated measure (g3)
+
 v 7: 4.April.2018 [JRH]
 	- use newer variable naming conventions
 	- retire unused word, bin setlow, sethigh, grp variables
 	- upgrade group-variables to double
 	- fix instructions
-
 
 v 7: 5.May.2013 [JRH]
 	- update usage of qsort to call external compare function xf_compare1_d
@@ -65,7 +68,7 @@ int main (int argc, char *argv[]) {
 	double *data=NULL,*tempdata=NULL;
 	/* arguments */
 	char *infile=NULL;
-	int setgint=0,setcolg1=1,setcolg2=2,setcolg3=3,setcoldata=4;
+	int setgint=0,setcolg1=1,setcolg2=2,setcolg3=3,setcoldata=4,setp=-1;
 
 	/* PRINT INSTRUCTIONS IF THERE IS NO FILENAME SPECIFIED */
 	if(argc<2) {
@@ -74,7 +77,8 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr,"%s\n",TITLE_STRING);
 		fprintf(stderr,"----------------------------------------------------------------------\n");
 		fprintf(stderr,"- Transpose data for mixed-design repeated measures analysis\n");
-		fprintf(stderr,"- Data can be NAN, but grouping variables must be finite numbers\n");
+		fprintf(stderr,"- Grouping variables 1 & 2 must be finite numerical values\n");
+		fprintf(stderr,"- Repeated-measures variable (-cg3) can be NAN\n");
 		fprintf(stderr,"- Max input line length = %d\n",MAXLINELEN);
 		fprintf(stderr,"USAGE:\n");
 		fprintf(stderr,"	%s [input] [g1] [g2] [g3] [data]\n",thisprog);
@@ -84,9 +88,11 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr,"	-cg2 column defining between-subjects groups  [%d]\n",setcolg2);
 		fprintf(stderr,"	-cg3 column defining repeated measure levels  [%d]\n",setcolg3);
 		fprintf(stderr,"	-cy column containing dependent variable [%d]\n",setcoldata);
-		fprintf(stderr,"	-gint: output groups as integers? (0=NO 1=YES) [%d]\n",setgint);
+		fprintf(stderr,"	-gint: output groups 1 & 2 as integers? (0=NO 1=YES) [%d]\n",setgint);
+		fprintf(stderr,"	-p: group3 (repeated) output precision [%d]\n",setp);
+		fprintf(stderr,"		-1=auto (%%f), 0=auto (%%g), >0=decimals\n");
 		fprintf(stderr,"NOTE: \n");
-		fprintf(stderr,"	- if there is no between-subjects grouping column, set -cg2 to 0\n");
+		fprintf(stderr,"	- if no between-subjects grouping column, set -cg2 to 0\n");
 		fprintf(stderr,"	- in this case, all subjects are placed in group \"1\"\n");
 		fprintf(stderr,"EXAMPLES:\n");
 		fprintf(stderr,"	%s data.txt -cg1 1 -cg2 2 -cg3 3 -cy 7\n",thisprog);
@@ -104,14 +110,14 @@ int main (int argc, char *argv[]) {
 	for(ii=2;ii<argc;ii++) {
 		if( *(argv[ii]+0) == '-') {
 			if((ii+1)>=argc) {fprintf(stderr,"\n--- Error[%s]: missing value for argument \"%s\"\n\n",thisprog,argv[ii]); exit(1);}
-			else if(strcmp(argv[ii],"-cg1")==0) 	{ setcolg1=atoi(argv[ii+1]); ii++;}
-			else if(strcmp(argv[ii],"-cg2")==0) 	{ setcolg2=atoi(argv[ii+1]); ii++;}
-			else if(strcmp(argv[ii],"-cg3")==0) 	{ setcolg3=atoi(argv[ii+1]); ii++;}
-			else if(strcmp(argv[ii],"-cy")==0) 	{ setcoldata=atoi(argv[ii+1]); ii++;}
-			else if(strcmp(argv[ii],"-gint")==0) 	{ setgint=atoi(argv[ii+1]); ii++;}
+			else if(strcmp(argv[ii],"-cg1")==0)  setcolg1= atoi(argv[++ii]);
+			else if(strcmp(argv[ii],"-cg2")==0)  setcolg2= atoi(argv[++ii]);
+			else if(strcmp(argv[ii],"-cg3")==0)  setcolg3= atoi(argv[++ii]);
+			else if(strcmp(argv[ii],"-cy")==0)   setcoldata= atoi(argv[++ii]);
+			else if(strcmp(argv[ii],"-gint")==0) setgint= atoi(argv[++ii]);
+			else if(strcmp(argv[ii],"-p")==0)    setp= atoi(argv[++ii]);
 			else {fprintf(stderr,"\t\a--- Error[%s]: invalid command line argument \"%s\"\n",thisprog,argv[ii]); exit(1);}
 	}}
-
 
 	sizeofdata= sizeof(*data);
 	sizeofgrp1= sizeof(*grp1);
@@ -170,14 +176,14 @@ int main (int argc, char *argv[]) {
 	qsort(listgrp3,nn,sizeof(double),xf_compare1_d);
 	/* copy only unique items to new version of listgrp3 */
 	aa=listgrp3[0]; for(ii=nlistgrp3=1;ii<nn;ii++) {if(listgrp3[ii]!=aa) listgrp3[nlistgrp3++]=listgrp3[ii];aa=listgrp3[ii]; }
-	//TEST:	for(ii=0;ii<nn;ii++) printf("%g\t%g\t%g\n",listgrp1[ii],listgrp2[ii],listgrp3[ii],data[ii]); exit(0);
+	//TEST:for(ii=0;ii<nn;ii++) printf("%g\t%g\t%g\t\t%g\n",listgrp1[ii],listgrp2[ii],listgrp3[ii],data[ii]); exit(0);
 	//TEST: printf("1:%ld\n2:%ld\n3:%ld\n",nlistgrp1,nlistgrp2,nlistgrp3);exit(0);
 
 
+	/* OUTPUT THE HEADER */
+	printf("subj\tgrp"); for(kk=0;kk<nlistgrp3;kk++) printf("\tr_%g",listgrp3[kk]); printf("\n");
+
 	/* TRANSPOSE THE DATA */
-	printf("subj\tgrp");
-	if(setgint==0) { for(kk=0;kk<nlistgrp3;kk++) printf("\tr_%ld",(long)listgrp3[kk]); printf("\n"); }
-	else           { for(kk=0;kk<nlistgrp3;kk++) printf("\tr_%ld",(long)listgrp3[kk]); printf("\n"); }
 	for(ii=0;ii<nlistgrp1;ii++) {
 		for(jj=0;jj<nlistgrp2;jj++) {
 			count=0;
@@ -189,7 +195,9 @@ int main (int argc, char *argv[]) {
 				count=0;
 				for(mm=0;mm<nn;mm++) {
 					if(grp1[mm]==listgrp1[ii] && grp2[mm]==listgrp2[jj] && grp3[mm]==listgrp3[kk]) {
-						printf("\t%f",data[mm]);
+						if(setp>0) printf("\t%.*f",setp,data[mm]);
+						else if(setp==0) printf("\t%g",data[mm]);
+						else printf("\t%f",data[mm]);
 						count++;
 					}
 				}
