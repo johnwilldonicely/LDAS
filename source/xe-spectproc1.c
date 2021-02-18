811610e9d4febbe17f231e3d589eb1a498f94836
JohnHuxter@MOLASSES-HP-Z440-Workstation <j.r.huxter@gmail.com>
2021-02-18 14:36:11 +0000

    xe-spectproc1 replaces xe-ldas5-spectproc1 - this is a general purpose program. Add option to use unadjusted spectrum for stats, and to output CSV list of band min-mid-max values

--- source/xe-spectproc1.c
+++ source/xe-spectproc1.c
@@ -1,4 +1,4 @@
-#define thisprog "xe-ldas5-spectproc1"
+#define thisprog "xe-spectproc1"
 #define TITLE_STRING thisprog" v 1: 30.May.2018 [JRH]"
 #define MAXLINELEN 1000
 
@@ -11,6 +11,10 @@
 /* <TAGS> signal_processing spectra filter transform </TAGS> */
 
 /*
+
+v 1: 18.February.2021 [JRH]
+	- use Butterworth filter instead of FIR for flattening the spectrum
+
 v 1: 30.May.2018 [JRH]
 	- improvement: use xf_geom_offset1_d to calculate relative peak amplitude
 
@@ -44,8 +48,7 @@ v 1: 18.February.2018 [JRH]
 /* external functions start */
 long *xf_lineparse2(char *line,char *delimiters, long *nwords);
 int xf_smoothgauss0_f(float *original,size_t arraysize,size_t index,int smooth,float *result);
-double *xf_filter_FIRcoef1(int ftaps, int fpass, double fomegac, double fbw, char *filtwintype, double fbeta, char *message);
-int xf_filter_FIRapply1_f(float *data, long nn, double *coefs, int ncoefs, int shift, char *message);
+int xf_filter_bworth1_f(float *X, size_t nn, float sample_freq, float low_freq, float high_freq, float res, char *message);
 long xf_detectinflect1_f(float *data,long n1,long **itime,int **isign,char *message);
 int xf_auc2_f(float *curvex, float *curvey, size_t nn, int ref, double *result ,char *message);
 double xf_geom_offset1_d(double x1,double y1,double x2,double y2, double x3, double y3, char *message);
@@ -71,8 +74,8 @@ int main (int argc, char *argv[]) {
 	double fbeta=0.0;
 	/* arguments */
 	char *infile,*setscreenlist=NULL;;
-	int setout=0, setverb=0;
-	double setdiv=2.0,setffreq=0.0125;
+	int setout=0,setverb=0,setuseadj=1;
+	double setdiv=2.0,setffreq=0.05;
 
 	out1="temp_"thisprog"_bands.txt\0";
 	out2="temp_"thisprog"_spect.txt\0";
@@ -83,33 +86,41 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr,"%s\n",TITLE_STRING);
 		fprintf(stderr,"----------------------------------------------------------------------\n");
 		fprintf(stderr,"Process neurophysiological power spectra \n");
-		fprintf(stderr,"- adaptive smoothing (more smoothing at higher frequencies)\n");
-		fprintf(stderr,"- FIR filtering to flatten the spectrum and reduce -ive AUC results\n");
-		fprintf(stderr,"- calculates AUC for peaks, relative to line connecting valleys\n");
-		fprintf(stderr,"USAGE: \n");
-		fprintf(stderr,"\t%s [infile]\n",thisprog);
-		fprintf(stderr,"	[infile]: file or stdin in format [freq] [power]\n");
+		fprintf(stderr,"- 1. adjusts spectrum: adaptive smoothing, more at higher frequencies\n");
+		fprintf(stderr,"- 2. adjusts spectrum: Butterworth HP filter to flatten the spectrum\n");
+		fprintf(stderr,"- 3. detects bands (if undefined) using +ive and -ive inflections\n");
+		fprintf(stderr,"- 4. gets stats (see below) for either orignal or adjusted spectrum\n");
+		fprintf(stderr,"\nUSAGE: \n");
+		fprintf(stderr,"    %s [infile]\n",thisprog);
+		fprintf(stderr,"        [infile]: 2-column file or stdin in format [freq] [power]\n");
 		fprintf(stderr,"VALID OPTIONS: defaults in []\n");
-		fprintf(stderr,"	-bands: CSV band-triplets (start,mid,stop) [unset=auto]\n");
-		fprintf(stderr,"	-out: stdout output (0= file-only, 1=spectrum, 2=bands) [%d]\n",setout);
-		fprintf(stderr,"	-verb: verbose output (0=NO 1=YES) [%d]\n",setverb);
-		fprintf(stderr,"...smoothing options...\n");
-		fprintf(stderr,"	-div: divisor in width= freq/div  (0=SKIP) [%g]\n",setdiv);
-		fprintf(stderr,"...filter options (high-pass FIR, 101 taps)...\n");
-		fprintf(stderr,"	-filt: HP filter frequency (0=SKIP) [%g]\n",setffreq);
+		fprintf(stderr,"    -bands: CSV band-triplets (start,mid,stop) [unset=auto]\n");
+		fprintf(stderr,"    -out: output options [%d]\n",setout);
+		fprintf(stderr,"        0= files-only\n");
+		fprintf(stderr,"        1= spectrum\n");
+		fprintf(stderr,"        2= band-definitions and measurements\n");
+		fprintf(stderr,"        3= CSV list of band definition triplets (min,mid,max)\n");
+		fprintf(stderr,"    -verb: verbose output (0=NO 1=YES) [%d]\n",setverb);
+		fprintf(stderr,"\n");
+		fprintf(stderr,"...spectral adjustment options...\n");
+		fprintf(stderr,"    -div: divisor in width= freq/div  (0=SKIP) [%g]\n",setdiv);
+		fprintf(stderr,"    -filt: HP filter frequency (0=SKIP) [%g]\n",setffreq);
+		fprintf(stderr,"    -useadj: use adjusted spectrum to calculate stats (0=NO 1=YES) [%d]\n",setuseadj);
+		fprintf(stderr,"    	- 0 avoids negative AUCs\n");
+		fprintf(stderr,"    	- 1 avoids negative relative values on steep slopes\n");
 		fprintf(stderr,"OUTPUT:\n");
-		fprintf(stderr,"	%s: the modified spectrum\n",out2);
-		fprintf(stderr,"	%s: stats for each detected band\n",out1);
-		fprintf(stderr,"		min mid max peak relpeak  auc aucpos aucneg  aucfull\n");
-		fprintf(stderr,"		min: low-frequency boundary for the band\n");
-		fprintf(stderr,"		mid: frequency at the band-peak\n");
-		fprintf(stderr,"		max: high-frequency boundary for the band\n");
-		fprintf(stderr,"		peak: absolute amplitude at the peak\n");
-		fprintf(stderr,"		relpeak: amplitude relative to line joining min-max\n");
-		fprintf(stderr,"		auc: half-width area-under-the-curve (maximises info)\n");
-		fprintf(stderr,"		aucpos: as above, positive-values only\n");
-		fprintf(stderr,"		aucneg: as above, negative-values only\n");
-		fprintf(stderr,"		aucful: complete AUC between min and max\n");
+		fprintf(stderr,"    %s: the adjusted spectrum\n",out2);
+		fprintf(stderr,"    %s: stats for each band\n",out1);
+		fprintf(stderr,"        min mid max peak relpeak  auc aucpos aucneg  aucfull\n");
+		fprintf(stderr,"        min: low-frequency boundary for the band\n");
+		fprintf(stderr,"        mid: frequency at the band-peak\n");
+		fprintf(stderr,"        max: high-frequency boundary for the band\n");
+		fprintf(stderr,"        peak: absolute amplitude at the peak\n");
+		fprintf(stderr,"        relpeak: amplitude relative to line joining min-max\n");
+		fprintf(stderr,"        auc: half-width area-under-the-curve (maximises info)\n");
+		fprintf(stderr,"        aucpos: as above, positive-values only\n");
+		fprintf(stderr,"        aucneg: as above, negative-values only\n");
+		fprintf(stderr,"        aucful: complete AUC between min and max\n");
 		fprintf(stderr,"----------------------------------------------------------------------\n");
 		fprintf(stderr,"\n");
 		exit(1);
@@ -127,9 +138,11 @@ int main (int argc, char *argv[]) {
 			else if(strcmp(argv[ii],"-verb")==0) setverb=atoi(argv[++ii]);
 			else if(strcmp(argv[ii],"-div")==0)  setdiv=atof(argv[++ii]);
 			else if(strcmp(argv[ii],"-filt")==0) setffreq=atof(argv[++ii]);
+			else if(strcmp(argv[ii],"-useadj")==0) setuseadj=atoi(argv[++ii]);
 			else {fprintf(stderr,"\n*** %s [ERROR: invalid command line argument \"%s\"]\n\n",thisprog,argv[ii]); exit(1);}
 	}}
-	if(setout<0 || setout>2) { fprintf(stderr,"\n--- Error [%s]: invalid -out [%d] must be 0-2\n\n",thisprog,setout);exit(1);}
+	if(setout<0 || setout>3) { fprintf(stderr,"\n--- Error [%s]: invalid -out [%d] must be 0-3\n\n",thisprog,setout);exit(1);}
+	if(setuseadj<0 || setuseadj>1) { fprintf(stderr,"\n--- Error [%s]: invalid -useadj [%d] must be 0-1\n\n",thisprog,setuseadj);exit(1);}
 	if(setverb!=0 && setverb!=1) { fprintf(stderr,"\n--- Error [%s]: invalid -verb [%d] must be 0 or 1\n\n",thisprog,setverb);exit(1);}
 
 
@@ -215,21 +228,13 @@ int main (int argc, char *argv[]) {
 
 	/*******************************************************************************
 	APPLY A HIGH-PASS FIR FILTER
-	- be careful if number of samples is less than default number of taps!
 	*******************************************************************************/
 	if(setffreq>0.0) {
-		if(ftaps>(nn/2)) ftaps=nn/2;
 		if(setverb==1) fprintf(stderr,"\t- filtering: HP=%g\n",setffreq);
-		fomegac= 2.0*(setffreq/spectfreq) ;
-		aa= 2.0*(3.0/spectfreq);
-		coefs= xf_filter_FIRcoef1(ftaps,fpass,fomegac,aa,"kaiser",fbeta,message);
-		if(coefs==NULL) { fprintf(stderr,"\n*** %s/%s\n\n",thisprog,message); exit(1); }
-		x= xf_filter_FIRapply1_f(amp2,nn,coefs,ftaps,1,message);
+		x= xf_filter_bworth1_f(amp2,nn,spectfreq,setffreq,0,sqrt(2),message);
 		if(x!=0) { fprintf(stderr,"\n*** %s/%s\n\n",thisprog,message); exit(1); }
-		free(coefs);
 	}
 	//TEST:	for(ii=0;ii<nn;ii++) printf("%g	%g\n",freq1[ii],amp2[ii]);exit(0);
-
 
 	/*******************************************************************************
 	AUTO-DEFINE BANDS BY DETECTING INFLECTIONS IN THE SPECTRUM
@@ -270,50 +275,74 @@ int main (int argc, char *argv[]) {
 	//TEST: for(ii=0;ii<nbands;ii++) {fprintf(stderr,"%ld	%ld	%ld\n",start1[ii],middle1[ii],stop1[ii]);}
 
 
+
 	/*******************************************************************************
-	CALCULATE THE LOCAL PEAK-AMPLITUDE AND AUC
+	SAVE THE MODIFIED SPECTRUM - FILE OR SCREEN
 	*******************************************************************************/
-	if(setverb==1) fprintf(stderr,"\t- writing band-stats: %s\n",out1);
-	if(setout==2) fpout=stdout;
-	else if((fpout=fopen(out1,"w"))==0) {fprintf(stderr,"\n--- Error[%s]: unable to write to file \"%s\"\n\n",thisprog,out1);exit(1);}
-	fprintf(fpout,"min\tmid\tmax\tpeak\trelpeak\tauc\taucpos\taucneg\taucfull\n");
-	for(ii=0;ii<nbands;ii++) {
-
-		/* temporarily store parameters for current peak */
-		min1= start1[ii];
-		mid1= middle1[ii];
-		max1= stop1[ii];
-
-		/* calculate the relative-amplitude of the peak (mid) */
-		peakamp= xf_geom_offset1_d(freq1[min1],amp2[min1],freq1[max1],amp2[max1],freq1[mid1],amp2[mid1],message);
-
-		/* calcualte the full AUC */
-		z= xf_auc2_f((freq1+min1),(amp2+min1),(1+max1-min1),1,result_d,message);
-		if(z<0) { fprintf(stderr,"\n*** %s/%s\n\n",thisprog,message); exit(1); }
-		auc1a= result_d[0]; auc1b= result_d[1]; auc1c= result_d[2];
-
-		/* calculate half-width AUC */
-		min2= mid1-(long)(((double)(mid1-min1))/2.0);
-		max2= mid1+(long)(((double)(max1-mid1))/2.0);
-		z= xf_auc2_f((freq1+min2),(amp2+min2),(1+max2-min2),1,result_d,message);
-		if(z<0) { fprintf(stderr,"\n*** %s/%s\n\n",thisprog,message); exit(1); }
-		auc2a= result_d[0]; auc2b= result_d[1]; auc2c= result_d[2];
-
-		/* output */
-		fprintf(fpout,"%g\t%g\t%g	%.4f\t%.4f	%.4f\t%.4f\t%.4f	%.4f\n",
-			freq1[min1],freq1[mid1],freq1[max1], amp2[mid1],peakamp, auc2a,auc2b,auc2c, auc1a);
+	if(setout==0 || setout==1) {
+		if(setverb==1) fprintf(stderr,"\t- writing spectrum:   %s\n",out2);
+		if(setout==1) fpout=stdout;
+		else if((fpout=fopen(out2,"w"))==0) {fprintf(stderr,"\n--- Error[%s]: unable to write to file \"%s\"\n\n",thisprog,out2);exit(1);}
+		fprintf(fpout,"freq\tsmoothed\n");
+		for(ii=0;ii<nn;ii++) fprintf(fpout,"%g\t%g\n",freq1[ii],amp2[ii]);
+		if(setout!=1) fclose(fpout);
 	}
-	if(setout!=2) fclose(fpout);
 
 	/*******************************************************************************
-	SAVE THE MODIFIED SPECTRUM
+	CALCULATE AND OUTPUT BAND PARAMETERS
 	*******************************************************************************/
-	if(setverb==1) fprintf(stderr,"\t- writing spectrum:   %s\n",out2);
-	if(setout==1) fpout=stdout;
-	else if((fpout=fopen(out2,"w"))==0) {fprintf(stderr,"\n--- Error[%s]: unable to write to file \"%s\"\n\n",thisprog,out2);exit(1);}
-	fprintf(fpout,"freq\tsmoothed\n");
-	for(ii=0;ii<nn;ii++) fprintf(fpout,"%g\t%g\n",freq1[ii],amp2[ii]);
-	if(setout!=1) fclose(fpout);
+	/* IF BAND-STATS ARE TO BE BASED ON ADJUSTED SPECTRUM (-useadj 1), COPY ORIGINAL AMP1 TO AMP2*/
+	if(setuseadj==0) for(ii=0;ii<nn;ii++) amp2[ii]= amp1[ii];
+	//TEST:	for(ii=0;ii<nn;ii++) printf("%ld\t%g\t%g\t%g\n",ii,freq1[ii],amp1[ii],amp2[ii]);
+
+	if(setout==0 || setout==2) {
+		if(setverb==1) fprintf(stderr,"\t- writing band-stats: %s\n",out1);
+		if(setout==2) fpout=stdout;
+		else if((fpout=fopen(out1,"w"))==0) {fprintf(stderr,"\n--- Error[%s]: unable to write to file \"%s\"\n\n",thisprog,out1);exit(1);}
+		fprintf(fpout,"min\tmid\tmax\tpeak\trelpeak\tauc\taucpos\taucneg\taucfull\n");
+
+		for(ii=0;ii<nbands;ii++) {
+
+			/* temporarily store parameters for current peak */
+			min1= start1[ii];
+			mid1= middle1[ii];
+			max1= stop1[ii];
+
+			/* calculate the relative-amplitude of the peak (mid) */
+			peakamp= xf_geom_offset1_d(freq1[min1],amp2[min1],freq1[max1],amp2[max1],freq1[mid1],amp2[mid1],message);
+
+			/* calcualte the full AUC */
+			z= xf_auc2_f((freq1+min1),(amp2+min1),(1+max1-min1),1,result_d,message);
+			if(z<0) { fprintf(stderr,"\n*** %s/%s\n\n",thisprog,message); exit(1); }
+			auc1a= result_d[0];
+			auc1b= result_d[1];
+			auc1c= result_d[2];
+
+			/* calculate half-width AUC */
+			min2= mid1-(long)(((double)(mid1-min1))/2.0);
+			max2= mid1+(long)(((double)(max1-mid1))/2.0);
+			z= xf_auc2_f((freq1+min2),(amp2+min2),(1+max2-min2),1,result_d,message);
+			if(z<0) { fprintf(stderr,"\n*** %s/%s\n\n",thisprog,message); exit(1); }
+			auc2a= result_d[0];
+			auc2b= result_d[1];
+			auc2c= result_d[2];
+
+			/* output options 0/2: bands and stats to file or stdout */
+			fprintf(fpout,"%g\t%g\t%g	%.4f\t%.4f	%.4f\t%.4f\t%.4f	%.4f\n",
+			freq1[min1],freq1[mid1],freq1[max1], amp2[mid1],peakamp, auc2a,auc2b,auc2c, auc1a);
+		}
+		if(setout==0) fclose(fpout);
+	}
+
+
+	/*******************************************************************************
+	OUTPUT OPTION3: CSV TRIPLET LISTS OF MIN,MID,MAX
+	*******************************************************************************/
+	if(setout==3){
+		 fprintf(fpout,"bands");
+		 for(ii=0;ii<nbands;ii++) fprintf(fpout,",%g,%g,%g",freq1[start1[ii]],freq1[middle1[ii]],freq1[stop1[ii]]);
+		 fprintf(fpout,"\n");
+	}
 
 
 	/*******************************************************************************
