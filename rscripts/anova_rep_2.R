8ef7c715e2c9638ce77bbfa0f256df6f9fc997c7
John Huxter <j.r.huxter@gmail.com>
2021-03-21 11:12:35 +0000

    Update anova_rep_2.R
    Add instructions
    Add brief output - round t & p values, remove "X" from names for times, and only report for p-values < .05

--- rscripts/anova_rep_2.R
+++ rscripts/anova_rep_2.R
@@ -1,32 +1,48 @@
-# 2-way repeated-measures ANOVA with contrasts against a control group 
-# to use, pipe 4 columns with headers to this script
-# column1= subject-id
-# column2= within-subjects group-number - assumes group "0" is the control group 
-# column3= within-subjects time
-# column4= result
+################################################################################
+# 2-WAY REPEATED-MEASURES ANOVA WITH CONTRASTS AGAINST A CONTROL GROUP
 
+# DEPENDENCIES: R, with packages emmeans and afex
+
+# USAGE:
+#    - Pipe 4 columns with headers to Rscript, calling this script
+#         - col1= subject-id
+#         - col2= within-subjects group-number - assumes group "0" is the control group
+#         - col3= within-subjects time
+#         - col4= result
+#    - Groups are assumed to be numeric and group 0 is taken as the control
+
+# EXAMPLE:
+# 	awk '{print $5,$1,$2,$3}' data.txt | Rscript [this script]
+################################################################################
 suppressMessages(library(afex))
 suppressMessages(library(emmeans))
-	
-df1= read.table("stdin", sep="\t", header=T)
 
+df1= read.table("stdin", sep="\t", header=T)
 namesub=colnames(df1)[1]
 namegrp=colnames(df1)[2]
 nametim=colnames(df1)[3]
 nameres=colnames(df1)[4]
 
-# 1 run the anova
+# 1 RUN THE ANOVA
 model= aov_ez(namesub, nameres, df1, within=c(namegrp,nametim), na.rm=FALSE)
 cat("\nANOVA\n\n")
 model
-
-# 2. get the estimated marginal means for each group
+# 2. GET THE ESTIMATED MARGINAL MEANS FOR EACH GROUP
 emm= emmeans(model,namegrp,by=nametim,model="multivariate")
-
-# 3. run the contrasts against the control group (0) 
+# 3. RUN THE CONTRASTS AGAINST THE CONTROL GROUP (0)
 con= contrast(emm, "trt.vs.ctrl", ref="X0",by=nametim,adjust="bon",parens=NULL)
+
+
+# GENERATE BRIEF REPORT
 cat("\nCONTRASTS\n\n")
-con
+brief= as.data.frame(con)
+# replace "X" in time-designations - should be in column 2 of te contrast output
+brief[,2]= gsub('X','',brief[,2])
+# select only significant contrasts
+brief= brief[brief$p.value<.05,]
+# round t & p values
+brief$p.value= round(brief$p.value,5)
+brief$t.ratio= round(brief$t.ratio,3)
 
-
-
+model
+brief
