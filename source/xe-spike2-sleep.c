24b84074a881afcdd26edfbad09bd6a10f67e98d
JohnHuxter@MOLASSES-HP-Z440-Workstation <j.r.huxter@gmail.com>
2021-06-13 16:57:44 +0100

    xe-spike2-sleep: added creation of inout filenames for EEG and EMG

--- source/xe-spike2-sleep.c
+++ source/xe-spike2-sleep.c
@@ -29,6 +29,9 @@ long xf_scale1_l(long data, long min, long max);
 int xf_bin3_d(double *data, short *flag, long setn, long setz, double setbinsize, char *message);
 double xf_bin1a_d(double *data, size_t *setn, size_t *setz, size_t setbins, char *message);
 int xf_timeconv1(double seconds, int *days, int *hours, int *minutes, double *sec2);
+
+char* xf_strsub1 (char *source, char *str1, char *str2);
+
 /* external functions end */
 
 int main (int argc, char *argv[]) {
@@ -46,15 +49,17 @@ int main (int argc, char *argv[]) {
 	struct tm *tstruct1;
 	int days,hours,minutes;
 	double seconds;
+
 	/* program-specific variables */
-	char *header=NULL,*pchar=NULL,*basename=NULL;
+	char *header=NULL,*pchar=NULL;
+	char *infileeeg=NULL,*infileemg=NULL;
 	int sizeofdata;
 	long *iword=NULL,nwords,binsamps,zero1,zero2;
-	double *data1=NULL,*pdata;
+	double *datact=NULL,*pdata;
 	double sampintact,samprateact,duract,binsize=10.0;
 
 	/* arguments */
-	char *infile=NULL;
+	char *infileact=NULL;
 	int setverb=0;
 	double setzero=0.0;
 
@@ -90,7 +95,7 @@ int main (int argc, char *argv[]) {
 	/********************************************************************************
 	READ THE FILENAME AND OPTIONAL ARGUMENTS
 	********************************************************************************/
-	infile= argv[1];
+	infileact= argv[1];
 	for(ii=2;ii<argc;ii++) {
 		if( *(argv[ii]+0) == '-') {
 			if((ii+1)>=argc) {fprintf(stderr,"\n--- Error[%s]: missing value for argument \"%s\"\n\n",thisprog,argv[ii]); exit(1);}
@@ -99,35 +104,37 @@ int main (int argc, char *argv[]) {
 			else {fprintf(stderr,"\n--- Error [%s]: invalid command line argument [%s]\n\n",thisprog,argv[ii]); exit(1);}
 	}}
 	if(setverb!=0 && setverb!=1) { fprintf(stderr,"\n--- Error[%s]: invalid -verb [%d] must be 0 or 1\n\n",thisprog,setverb);exit(1);}
-	if(strcmp(infile,"stdin")==0) { fprintf(stderr,"\n--- Error[%s]: this program does not accept \"stdin\" as an input. Please specify a filename\n\n",thisprog);exit(1);}
+	if(strcmp(infileact,"stdin")==0) { fprintf(stderr,"\n--- Error[%s]: this program does not accept \"stdin\" as an input. Please specify a filename\n\n",thisprog);exit(1);}
 
 
 	/********************************************************************************
-	CHECK FILENAME AND GENERATE BASENAME FOR READING OTHER FILES
+	CHECK ACTIVITY FILENAME AND GENERATE FILENAMES FOR EEG AND EMG
 	********************************************************************************/
-	pchar= strstr(infile,"ACT_"); // infile must contain the word "activity.txt"
-	if(pchar==NULL) { fprintf(stderr,"\n--- Error[%s]: invalid infile [%s] - must include the keyword \"ACT_\"\n\n",thisprog,infile);exit(1);}
-	ii= pchar-infile; // the position at which "activity" is found in the filename
-	basename= realloc(basename,strlen(infile));
-	if(basename==NULL) {fprintf(stderr,"\n--- Error[%s]: insufficient memory\n\n",thisprog);exit(1);};
-	strncpy(basename,infile,ii);
+	pchar= strstr(infileact,"ACT_"); // infileact must contain the word "activity.txt"
+	if(pchar==NULL) { fprintf(stderr,"\n--- Error[%s]: invalid infileact [%s] - must include the keyword \"ACT_\"\n\n",thisprog,infileact);exit(1);}
+	infileeeg= xf_strsub1(infileact,"ACT","EEG"); if(infileeeg==NULL) {fprintf(stderr,"\n--- Error[%s]: insufficient memory\n\n",thisprog);exit(1);};
+	infileemg= xf_strsub1(infileact,"ACT","EMG"); if(infileemg==NULL) {fprintf(stderr,"\n--- Error[%s]: insufficient memory\n\n",thisprog);exit(1);};
+
 	fprintf(stderr,"\n");
-	fprintf(stderr,"...infile= %s\n",infile);
-	fprintf(stderr,"...basename= %s\n",basename);
+	fprintf(stderr,"...activity file= %s\n",infileact);
+	fprintf(stderr,"...matching EEG=  %s\n",infileeeg);
+	fprintf(stderr,"...matching EMG=  %s\n",infileemg);
 
-
+	/********************************************************************************/
 	/********************************************************************************
 	STORE ACTIVITY DATA
 	- probably collected at 1Hz
 	- immobility is registered as zero
 	- max mobility is probably ~6
 	********************************************************************************/
+	/********************************************************************************/
+
 	if(setverb>0) fprintf(stderr,"...reading ACTIVITY data...\n");
-//	data1= xf_readtable1_d(infile,"\t",&ncols,&nrows,&header,message);
-	data1= xf_readspike2_text_d(infile,&nn,&sampintact,message);
-	if(data1==NULL) { fprintf(stderr,"\n--- Error: %s/%s\n\n",thisprog,message); exit(1); }
+//	datact= xf_readtable1_d(infileact,"\t",&ncols,&nrows,&header,message);
+	datact= xf_readspike2_text_d(infileact,&nn,&sampintact,message);
+	if(datact==NULL) { fprintf(stderr,"\n--- Error: %s/%s\n\n",thisprog,message); exit(1); }
 	samprateact= 1.0/sampintact;
-	//TEST:	printf("header: %s",header); for(ii=0;ii<nrows;ii++) { pdata= data1+(ii*ncols); printf("%g",pdata[0]); for(jj=1;jj<ncols;jj++) printf("\t%g",pdata[jj]); printf("\n"); }
+	//TEST:	printf("header: %s",header); for(ii=0;ii<nrows;ii++) { pdata= datact+(ii*ncols); printf("%g",pdata[0]); for(jj=1;jj<ncols;jj++) printf("\t%g",pdata[jj]); printf("\n"); }
 
 	/* FIND DURATION N SECONDS AND REPORT */
 	duract= nn*sampintact;
@@ -138,16 +145,23 @@ int main (int argc, char *argv[]) {
 	fprintf(stderr,"        duration= %g seconds (%02d:%02d:%02d:%.3f)\n",duract,days,hours,minutes,seconds);
 
 	/* RECTIFY: because the DSI receiver system creates brief 1s negativities either side of periods of activity */
-	for(ii=0;ii<nn;ii++) if(data1[ii]<0.0) data1[ii]*=-1.0;
+	for(ii=0;ii<nn;ii++) if(datact[ii]<0.0) datact[ii]*=-1.0;
 
 	/* AVERAGE THE DATA IN 10 SECOND BINS (EPOCHS) */
 	zero1= (long)(setzero*samprateact);
-	z= xf_bin1b_d(data1,&nn,&zero1,binsize,message);
+	z= xf_bin1b_d(datact,&nn,&zero1,binsize,message);
+
+	/********************************************************************************/
+	/********************************************************************************/
+	/* EMG DATA: STORE AND PROCESS */
+	/********************************************************************************/
+	/********************************************************************************/
 
 
-	for(ii=0;ii<nn;ii++) {
-		printf("%g\n",data1[ii]);
-	}
+
+
+
+	//TEST for(ii=0;ii<nn;ii++) { printf("%g\n",datact[ii]); }
 
 	exit(0);
 
@@ -170,10 +184,11 @@ goto END;
 END:
 	if(setverb>0) fprintf(stderr,"...complete!\n");
 
-	if(basename!=NULL) free(basename);
+	if(infileeeg!=NULL) free(infileeeg);
+	if(infileemg!=NULL) free(infileemg);
 	if(line!=NULL) free(line);
 	if(iword!=NULL) free(iword);
-	if(data1!=NULL) free(data1);
+	if(datact!=NULL) free(datact);
 	if(header!=NULL) free(header);
 	exit(0);
 }
